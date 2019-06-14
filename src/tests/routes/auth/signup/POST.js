516f0b43a3c9a5d309b5063e4efe197d916f43a2
Aditya Pillai <github@pillai.io>
2019-06-14 15:53:29 -0500

    Finished POST /auth/signup testing

--- src/tests/routes/auth/signup/POST.js
+++ src/tests/routes/auth/signup/POST.js
@@ -6,22 +6,20 @@ const knex = require('../../../helpers/knex');
 
 require('./../../GET');
 
-describe('POST /auth/signup', () => {
+const POST_SIGNUP_URL = '/auth/signup';
+
+describe(`POST ${POST_SIGNUP_URL}`, () => {
   before((done) => {
     knex.migrate.rollback()
+      .then(() => knex.migrate.latest())
       .then(() => {
-        console.log('Finished rollback...');
-        return knex.migrate.latest();
-      })
-      .then(() => {
-        console.log('Finished migrations...');
         done();
       });
   });
 
   it('200 Valid Signup', (done) => {
     request(app)
-      .post('/auth/signup')
+      .post(POST_SIGNUP_URL)
       .send({
         email: 'aditya@example.com',
         password: 'example_password',
@@ -33,6 +31,92 @@ describe('POST /auth/signup', () => {
       .expect(200, {
         success: true,
         message: 'You have successfully created an account.',
+      }, done);
+  });
+
+  it('409 User Account Already Exists', (done) => {
+    request(app)
+      .post(POST_SIGNUP_URL)
+      .send({
+        email: 'aditya@example.com',
+        password: 'example_password',
+        firstName: 'Aditya',
+        lastName: 'Pillai',
+        'g-recaptcha-response': 'recaptcha-token',
+      })
+      .expect('Content-Type', /json/)
+      .expect(409, {
+        success: false,
+        error: 'user-already-exists',
+        message: 'A user with this email address already exists.',
+      }, done);
+  });
+
+  it('400 Password Too Short', (done) => {
+    request(app)
+      .post(POST_SIGNUP_URL)
+      .send({
+        email: 'aditya@example.com',
+        password: 'short',
+        firstName: 'Aditya',
+        lastName: 'Pillai',
+        'g-recaptcha-response': 'recaptcha-token',
+      })
+      .expect('Content-Type', /json/)
+      .expect(400, {
+        success: false,
+        error: 'password-too-short',
+        message: 'Please provide a password of length 8+.',
+      }, done);
+  });
+
+  it('400 Email Not Provided', (done) => {
+    request(app)
+      .post(POST_SIGNUP_URL)
+      .send({
+        password: 'example_password',
+        firstName: 'Aditya',
+        lastName: 'Pillai',
+        'g-recaptcha-response': 'recaptcha-token',
+      })
+      .expect('Content-Type', /json/)
+      .expect(400, {
+        success: false,
+        error: 'no-email',
+        message: 'Please provide your email.',
+      }, done);
+  });
+
+  it('400 Password Not Provided', (done) => {
+    request(app)
+      .post(POST_SIGNUP_URL)
+      .send({
+        email: 'aditya@example.com',
+        firstName: 'Aditya',
+        lastName: 'Pillai',
+        'g-recaptcha-response': 'recaptcha-token',
+      })
+      .expect('Content-Type', /json/)
+      .expect(400, {
+        success: false,
+        error: 'no-password',
+        message: 'Please provide a password',
+      }, done);
+  });
+
+  it('400 Name Not Provided', (done) => {
+    request(app)
+      .post(POST_SIGNUP_URL)
+      .send({
+        email: 'aditya@example.com',
+        password: 'example_password',
+        'g-recaptcha-response': 'recaptcha-token',
+      })
+      .expect('Content-Type', /json/)
+      .expect(400, {
+        success: false,
+        error: 'no-name',
+        message: 'Please provide both your first and last name.',
       }, done);
   });
 });
